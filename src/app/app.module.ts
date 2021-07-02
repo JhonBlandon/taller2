f48e7c034e0f9042fea1438907739227f3a15952
tecnialarmas <tecnialarmas@iMac-de-tecnialarmas.local>
2021-07-01 19:32:01 -0500

    completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { FacturaComponent } from './factura/factura.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    FacturaComponent
   ],
   imports: [
     BrowserModule
